6c4dc807818e1ac2dcce3ca3df6778d5c57ea4f2
Charlie <cnewlinds@gmail.com>
2023-09-08 18:22:22 +1000

    add public & private key

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let button = document.querySelector('#search-button');
 let displayContainer = document.querySelector('#display-container');
 let listContainer = document.querySelector('#search-list');
 let ts = '1694160786735'
-let publicKey = ""
+let publicKey = "7086793dcb0eb11d2088ebb2002b331f"
 let hashVal = ""
 
 let date = new Date();
@@ -12,3 +12,6 @@ console.log(date.getTime());
 const [timestamp, apiKey, hashValue] = [ts, apiKeyVal, hashVal]
 
 // https://www.youtube.com/watch?v=8se1rBs--4A&list=PLNCevxogE3fiLT6bEObGeVfHVLnttptKv&index=16&ab_channel=CodingArtist 
+// public key: "7086793dcb0eb11d2088ebb2002b331f
+// private key: "5fe433ed0d3f5aa5ef255cdda722446877aa2a57"
+"
