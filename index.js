ffeb7f2904e7dc184ab947e4fd9da60b875e069b
root <root@DESKTOP-2B2KLNC.localdomain>
2023-09-14 17:17:51 +1000

    add Avengers filter

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let displayContainer = document.querySelector('#display-container');
 let listContainer = document.querySelector('#search-list');
 let heroIndex = document.querySelector('#hero-index');
 let heroCard = document.querySelector('.hero-card')
+const heroGrid = document.querySelector('#hero-grid'); 
 
 
 let ts = '1694407684017'
@@ -80,8 +81,6 @@ button.addEventListener(
     const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}`;
     const response = await fetch(url);
     const jsonData = await response.json(); 
-    const heroGrid = document.querySelector('#hero-grid'); // Assuming you have a container with the id "hero-grid"
-
     jsonData.data["results"].forEach((element) => {
         const card = document.createElement('div');
         card.className = "hero-card";
@@ -103,10 +102,74 @@ button.addEventListener(
 fetchSuperHero();
 
 
-///Filter section
+///Avngers Filter
+const avengersHeroes = [
+  "Iron Man",
+  "Captain America",
+  "Thor",
+  "Black Widow",
+  "Black Panther",
+  "Hawkeye",
+  "Loki",
+  "Nick Fury",
+  "Thanos",
+  "Wasp"
+];
+
+async function fetchAvengers() {
+  for (const heroName of avengersHeroes) {
+    const url = `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${heroName}`;
+
+    try {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      jsonData.data["results"].forEach((element) => {
+        const card = document.createElement('div');
+        card.className = "hero-card";
+        
+        const img = document.createElement('img');
+        img.src = `${element.thumbnail["path"] + "." + element.thumbnail["extension"]}`;
+        img.alt = element.name;
+        img.className = "index-img";
+        
+        const name = document.createElement('p');
+        name.innerHTML = element.name;
+        name.className = "character-name";
+
+        card.append(img, name);
+        heroGrid.append(card);
+  
+      })
+    } catch (error) {
+      console.error(`Error fetching data for ${heroName}: ${error}`);
+    }
+  }
+}
+
+const teamSelect = document.querySelector('#team-select')
+
+teamSelect.addEventListener('change', function () {
+  const selectedTeam = teamSelect.value;
+
+  if (selectedTeam === 'Avengers') {
+  
+    heroGrid.innerHTML = '';
+
+    
+    fetchAvengers();
+  }
+});
 
 
-// https://www.youtube.com/watch?v=8se1rBs--4A&list=PLNCevxogE3fiLT6bEObGeVfHVLnttptKv&index=16&ab_channel=CodingArtist 
+
+
+
+
+
+
+
+
+
 // public key: "7086793dcb0eb11d2088ebb2002b331f
 // private key: "5fe433ed0d3f5aa5ef255cdda722446877aa2a57"
 
